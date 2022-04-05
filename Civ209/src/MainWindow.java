9ecd7b67db9bfc71fbe68bb8b4698f6e7b536cb7
Emily Bronkema <ebron592@students.bju.edu>
2022-04-05 14:33:34 -0400

    it's question

--- Civ209/src/MainWindow.java
+++ Civ209/src/MainWindow.java
@@ -4,7 +4,7 @@ import javafx.scene.control.*;
 import javafx.scene.control.Alert.AlertType;
 
 /*
-To do or not to do, that is the answer.
+To do or not to do, that is the question.
 */
 public class MainWindow {
 
