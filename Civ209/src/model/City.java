a03df5abb77db849fe439693d109622c57f23152
Emily <ebron592@students.bju.edu>
2022-04-26 14:42:34 -0400

    projectile speed

--- Civ209/src/model/City.java
+++ Civ209/src/model/City.java
@@ -161,7 +161,7 @@ public class City extends Entity {
             });
             for (Troop troop : troops) {
                 if (troop.getNationality() != nationality && getLocation().isNearThis(troop.getLocation())) {
-                    if (turnCount % 10 == 0) {
+                    if (turnCount % 15 == 0) {
                         Troop targettroop = troop;
                         projectile = new Projectile(this.location, turnCount, 2, 0,
                                 targettroop.getLocation(), 2);
