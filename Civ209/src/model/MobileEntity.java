198808aaa99dcfb1dc485bdaf90f5931fee68420
Rhys Fuller <rfull917@students.bju.edu>
2022-04-27 14:47:46 -0400

    documentation

--- Civ209/src/model/MobileEntity.java
+++ Civ209/src/model/MobileEntity.java
@@ -14,6 +14,14 @@ public class MobileEntity extends Entity {
     private double heading;
     private Coordinate destination;
 
+    /**
+     * Basic constructor
+     * @param location - coordinate to put mobile entity at to begin
+     * @param turnCount - how many times update has been called
+     * @param speed - speed at which entity moves
+     * @param heading - heading at which it's moving
+     * @param destination - coordinate that entity is heading to.
+     */
     public MobileEntity(Coordinate location, int turnCount, double speed, double heading,
             Coordinate destination) {
         super(location, turnCount);
@@ -33,6 +41,9 @@ public class MobileEntity extends Entity {
         super.update();
     }
 
+    /**
+     * Logic to move mobile entity.
+     */
     public void move() {
         getLocation()
                 .setX(getLocation().getX() + (speed / (Constants.tickSpeed / 10)) * Math.cos(heading * Math.PI / 180));
@@ -40,6 +51,9 @@ public class MobileEntity extends Entity {
                 .setY(getLocation().getY() + (speed / (Constants.tickSpeed / 10)) * Math.sin(heading * Math.PI / 180));
     }
 
+    /******************************************************/
+    // Getters and setters
+    
     public double getSpeed() {
         return speed;
     }
--- Civ209/src/model/WeatherType.java
+++ Civ209/src/model/WeatherType.java
@@ -9,5 +9,4 @@ public enum WeatherType {
     LightningStorm,
     Blizzard,
     Flood,
-    Drought
 }
